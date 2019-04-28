590edb062cd46adb00c28c53f3243827556868e1
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-04-28 20:17:18 -0300

    Screens doctors and patients

---
+++ models/Agendamento.js
@@ -0,0 +1,13 @@
+
+
+
+/* 
+
+VERIFICAR NO SITE ABAIXO UMA FORMA DE INSERIR VARIOS MODELS JUNTOS 
+
+https://pt.stackoverflow.com/questions/131463/mongoose-objeto-%C3%BAnico-em-um-array */
+
+/* 
+MONGOOSE MATERIAL
+
+http://learn.ironhack.com/#/learning_unit/6482 */
