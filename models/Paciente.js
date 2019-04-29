f7389c30dae2b5e7ef7364f323eae0ef56d0487e
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-04-29 13:13:14 -0300

    Createddoctors on database

--- models/Paciente.js
+++ models/Paciente.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const PatientSchema = new Schema({
-    email: { type: String},
-    password: { type: String}
-    name: { type: String},
-    CPF: { type: String},
-    birthday: { type: String},
-    telResidencial: { type: Number},
-    cellphone: { type: Number},
-    healthInsurance: { type: Number},
+  email: { type: String },
+  password: { type: String },
+  name: { type: String },
+  CPF: { type: String },
+  birthday: { type: String },
+  telResidencial: { type: Number },
+  cellphone: { type: Number },
+  healthInsurance: { type: Number },
 });
 
 const Patient = mongoose.model('Patient', PatientSchema);
--- routes/medico.js
+++ routes/medico.js
@@ -4,22 +4,65 @@ const express = require('express');
 const router = express.Router(); //  = authRoutes const authRoutes = express.Router();
 
 // Medico model
+const bcrypt = require('bcrypt');
 const Medico = require('../models/Medico');
 
 // Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt");
 
 const bcryptSalt = 10;
 
-
-router.get('/cadastro', (req, res) => {         // /signup
+router.get('/cadastro', (req, res) => {
+  // /signup
   res.render('./medicos/cadastro-medico');
 });
 
+router.post('/cadastro', (req, res, next) => {
+  const {
+    username, password, name, CRM, specialty, telResidencial, cellphone,
+  } = req.body;
 
+  console.log('TEXTO ASSIM', username);
 
+  if (username === '' || password === '') {
+    res.render('/cadastro', { message: 'Indicate email and password' });
+    return;
+  }
 
+  Medico.findOne({ username })
+    .then((user) => {
+      console.log('caiu no then do find Medico')
+      if (user !== null) {
+        res.render('/cadastro', { message: 'The username already exists' });
+        return;
+      }
 
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newDoctor = new Medico({
+        username,
+        password: hashPass,
+        name,
+        CRM,
+        specialty,
+        telResidencial,
+        cellphone,
+      });
+
+      newDoctor.save((err) => {
+        if (err) {
+          console.log(err)
+          res.redirect('/cadastro');
+        } else {
+          console.log('Medico salvo')
+          res.redirect('/');
+        }
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 
 // Routes protected
